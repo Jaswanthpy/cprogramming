4b2d39a8e49017dbd337c1798a5cc288b8678974
Jaswanth Reddy <jaswanthr@azuga.com>
2023-05-23 20:58:30 +0530

    ftoc

--- ftoc.c
+++ ftoc.c
@@ -8,7 +8,7 @@ int main(void) {
     int step = 20;
 
     printf("Fahrenheit\tCelsius\n");
-    fahr = (double)lower
+    fahr = (double)lower;
     while (fahr <= double(upper))
     {
         celcius = (5.0 / 9.0) * (fahr - 32.0);
